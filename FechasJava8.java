d5afabb3cf7985ffeaf6c4e880560dc1e970fb20
Your Name <y@m.es>
2019-01-23 12:32:33 +0100

    Creación de fechas y obtención de campos

--- FechasJava8.java
+++ FechasJava8.java
@@ -5,10 +5,35 @@
  */
 package javatime;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  *
  * @author usuario
  */
 public class FechasJava8 {
+    
+    public static void main(String[] args) {
+        
+        /* CREACIÓN DE FECHAS Y OBTENCIÓN DE CAMPOS*/
+        
+        // Fecha actual
+        LocalDate hoy = LocalDate.now();
+        System.out.println("Hoy es " + hoy);
+        
+        // Obtener campos individualmente
+        System.out.println("El año es " + hoy.getYear());
+        System.out.println("El mes es " + hoy.getMonthValue());
+        System.out.println("El dia es " + hoy.getDayOfMonth());
+        
+        // Creación de fechas concretas
+        LocalDate nacimientoJose = LocalDate.of(1999, 3, 8);
+        System.out.println("Jose es un mentiroso " + nacimientoJose);
+        
+        LocalDate nacimientoRosa = LocalDate.of(1988, Month.APRIL, 13);
+        System.out.println("Rosa también miente " + nacimientoRosa);
+        
+    }
     
 }
