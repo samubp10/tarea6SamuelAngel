082dae4c94b9b58e9931127500e4117091392e22
Your Name <y@m.es>
2019-01-24 13:03:28 +0100

    EJemplo clase YearMonth

--- FechasJava8.java
+++ FechasJava8.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 // Enumerado para los meses del año (Enero, ...)
 import java.time.Month;
 import java.time.MonthDay;
+import java.time.YearMonth;
 
 /**
  *
@@ -47,12 +48,30 @@ public class FechasJava8 {
         
         // Método isBefore()
         
-        /* EJEMPLO CON LA CLASE MOTHDAY */
-        
+        /* EJEMPLO CON LA CLASE MONTHDAY */
+        // Sólo guarda el día y el mes (no el año)
         LocalDate diaAnda = LocalDate.of(1976, Month.FEBRUARY, 28);
         
         MonthDay diaMesAndalucia = MonthDay.of(diaAnda.getMonth(), diaAnda.getDayOfMonth());
         MonthDay diaMesHoy = MonthDay.from(LocalDate.now());
+        
+        if (diaMesAndalucia.equals(diaMesHoy)){
+            System.out.println("Hoy es el día de Andalucía");
+            
+        }
+         
+        /* EJEMPLO CON LA CLASE YEARMONTH */
+        // Sólo guarda el año y el mes (no el día)
+        
+        YearMonth caducidadTarjeta = YearMonth.of(2019, Month.MARCH);
+        System.out.println("Su tarjeta caduca en " + caducidadTarjeta);
+        YearMonth actual = YearMonth.from(LocalDate.now());
+        
+        if (caducidadTarjeta.isBefore(actual)){
+            System.out.println("Su tarjeta ha caducado");
+        }
+        
+        
     }
     
 }
